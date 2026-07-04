f988fa4c8ca60658c47897e60b0150b26053d22e
akerfatih34 <144621435+akerfatih34@users.noreply.github.com>
2026-07-04 20:13:39 +0300

    v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "neuromind-v2";
+const CACHE = "neuromind-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -8,11 +8,23 @@ self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match("./index.html")))
-  );
+  const isPage = e.request.mode === "navigate" || e.request.destination === "document";
+  if (isPage) {
+    // önce ağ: güncellemeler anında gelsin; çevrimdışıysa önbellek
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }))
+    );
+  }
 });
