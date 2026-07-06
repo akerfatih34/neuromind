bdf9658be08c52cd91078414612d9a76b701c7f8
akerfatih34 <144621435+akerfatih34@users.noreply.github.com>
2026-07-06 22:33:41 +0300

    oyun ekleme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "neuromind-v3";
+const CACHE = "neuromind-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
