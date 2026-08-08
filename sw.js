4b313bf247bdf1fbec55f40c9030fd82e6032382
akerfatih34 <144621435+akerfatih34@users.noreply.github.com>
2026-08-09 00:32:43 +0300

    v1.6.3: Safari alt bant düzeltmesi
    v1.6.3: Safari alt bant düzeltmesi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "neuromind-v9";
+const CACHE = "neuromind-v10";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
